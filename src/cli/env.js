7283e16c6b1c2fe6955e60e0fe06950bbbb5eda0
Vladimir <kusakinvova@gmail.com>
2024-01-29 14:18:07 +0300

    fix: check cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,7 +1,7 @@
 const parseEnv = () => {
   const processEnv = process.env;
   const filterResult = Object.entries(processEnv).filter((value) => value[0].indexOf('RSS_') === 0);
-  const stringResult = filterResult.map((value) => `${value[0]}=${value[1]}`).join(', ');
+  const stringResult = filterResult.map((value) => `${value[0]}=${value[1]}`).join('; ');
   console.log(stringResult);
 };
 
