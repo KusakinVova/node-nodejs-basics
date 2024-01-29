0eb408138ecd1230767e2f78aac4e1f8224def84
Vladimir <kusakinvova@gmail.com>
2024-01-29 17:57:49 +0300

    feat: stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const fileName = 'fileToRead.txt';
+  const sourceFolderName = 'files';
+  const filePath = path.join(__dirname, sourceFolderName, fileName);
+  try {
+    const stream = fs.createReadStream(filePath, 'utf8');
+
+    stream.on('data', (data) => {
+      process.stdout.write(data);
+    });
+
+    stream.on('error', (error) => {
+      console.error(error);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+  // Write your code here
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,34 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const fileName = 'fileToWrite.txt';
+  const sourceFolderName = 'files';
+  const filePath = path.join(__dirname, sourceFolderName, fileName);
+
+  try {
+    const writeStream = fs.createWriteStream(filePath, { flags: 'a' });
+
+    process.stdin.on('data', (data) => {
+      writeStream.write(data);
+    });
+    process.stdin.on('end', () => {
+      writeStream.end();
+    });
+    process.stdin.on('error', (error) => {
+      console.error('Error of reading from stdin:', error.message);
+    });
+
+    writeStream.on('error', (error) => {
+      console.error(`Error of  write in file "${fileName}":`, error.message);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await write();
