614d381662c817b41aad4f06f4ed1e6ac2e1501a
Vladimir <kusakinvova@gmail.com>
2024-01-29 09:33:03 +0300

    feat: add cli-function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,28 @@
+const getArgs = (args) => {
+  const res = {};
+  const [execute, file, ...rest] = args;
+  rest.forEach((value, index, array) => {
+    if (value.charAt(0) === '-') {
+      if (index == array.length - 1) {
+        res[value.substring(2)] = true;
+      } else if (array[index + 1].charAt(0) !== '--') {
+        res[value.substring(2)] = array[index + 1];
+      } else {
+        res[value.substring(2)] = true;
+      }
+    }
+  });
+  return res;
+};
+
 const parseArgs = () => {
-    // Write your code here 
+  const args = getArgs(process.argv);
+
+  const resultString = Object.entries(args)
+    .map(([key, value]) => `${key} is ${value}`)
+    .join(', ');
+
+  console.log(resultString);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+  const processEnv = process.env;
+  const filterResult = Object.entries(processEnv).filter((value) => value[0].indexOf('RSS_') === 0);
+  const stringResult = filterResult.map((value) => `${value[0]}=${value[1]}`).join(', ');
+  console.log(stringResult);
 };
 
 parseEnv();
